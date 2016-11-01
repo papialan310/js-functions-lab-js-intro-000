25e7881d21583d21d5fd75989bf95be93261655e
papialaan310 <alan.alejandro911@gmail.com>
2016-11-01 17:38:00 +0000

    I'm ready to send my files to github.

---
+++ index.js
@@ -0,0 +1,14 @@
+// write your code below!
+var happyHolidays = function(happyHolidays){
+  return "Happy holidays!";
+};
+var happyHolidaysTo = function(name){
+ return "Happy holidays, you!"
+
+}
+var  happyHolidayTo = function(Independence){
+  return "Happy Independence Day, you!";
+}
+var holidayCountdown = function(countdown){
+  return "It's 20 days until Mother's Day!"
+}
